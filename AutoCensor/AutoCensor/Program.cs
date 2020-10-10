90175b55f7b244f010a32cd897768379da2cb1b4
PopsicleTreehouse <54643196+PopsicleTreehouse@users.noreply.github.com>
2020-10-10 13:37:29 -0700

    Add files via upload

---
+++ AutoCensor/AutoCensor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace AutoCensor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] swears = { "fuck", "shit", "ass", "crap", "bitch", "pussy", "dick", "cunt", "asshole", "bitchass", "shitface", "dumbass" };
+            Console.Write("Enter text you want to censor: ");
+            string entryString = Console.ReadLine();
+            string[] entryStringParser = entryString.Split(' ');
+            foreach (var entryStringParsed in entryStringParser)
+            {
+                bool stringIsSwear = true;
+                if (stringIsSwear)
+                {
+                        if (!swears.Any(entryStringParsed.Contains))
+                        {
+                            Console.Write($"{entryStringParsed} ");
+                        }
+                        if (swears.Any(entryStringParsed.Contains))
+                        {
+                        StringBuilder entryStringParsedCensored = new StringBuilder(entryStringParsed);
+                        entryStringParsedCensored.Replace(entryStringParsedCensored[1], '*');
+                        Console.Write($"{entryStringParsedCensored} ");
+                        }
+                    }
+                }
+            }
+        }
+    }
---
+++ BeeMovieScript/BeeMovieScript/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace BeeMovieScript
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(File.ReadAllText("/Users/brandonsmacbookpro/Documents/BeeMovieScript.rtf"));
+            Console.ReadKey(true);
+        }
+    }
+}
