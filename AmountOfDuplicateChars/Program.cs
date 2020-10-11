5e67e538c2f33ed5758094fd2e23b30099dea783
Brandon Yao <ahoynodnarb@gmail.com>
2020-10-11 10:48:51 -0700

    Added files...

---
+++ AmountOfDuplicateChars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AmountOfDuplicateChars
+{
+    class Program
+    {
+        static int AmountOfDuplicates(string inputString, char duplicateChar)
+        {
+            int amountOfDupes = 0;
+            foreach (char ch in inputString)
+            {
+                if (ch == duplicateChar)
+                    amountOfDupes++;
+            }
+            return amountOfDupes;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input string: ");
+            string inputString = Console.ReadLine();
+            Console.WriteLine("Input duplicate char: ");
+            char duplicateChar = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(AmountOfDuplicates(inputString, duplicateChar));
+        }
+    }
+}
