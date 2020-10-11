bf1e8af30cbdcf7dc3c89af1ece36d9867550c8f
Booper Dooper <ahoynodnarb@gmail.com>
2020-10-10 23:40:26 -0700

    Added some files...

---
+++ NonRepeatedValue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NonRepeatedValue
+{
+    class Program
+    {
+        static string returnFirstDistinct(int[] comparedArray) {
+            List<int> comparedList = comparedArray.ToList<int>();
+            comparedList.Sort();
+            for(int i = 1; i < comparedList.Count; i++) {
+                if(comparedList[i] != comparedList[i - 1])
+                    return "The first unique integer was " + comparedList[i - 1].ToString();
+            }
+            return "There were no duplicate integers";
+        }
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[3];
+            for(int i = 0; i < intArray.Length; i++) {
+                Console.WriteLine("Input numbers: ");
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(returnFirstDistinct(intArray));
+        }
+    }
+}
