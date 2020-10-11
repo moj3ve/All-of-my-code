e1da517518e90adfb6a02e2b1e3221ed918d2cd3
Booper Dooper <ahoynodnarb@gmail.com>
2020-10-10 23:58:40 -0700

    Added some files...

---
+++ OnlyDigitString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OnlyDigitString
+{
+    class Program
+    {
+        static bool onlyHasInt(string inputString)
+        {
+            try
+            {
+                Convert.ToInt16(inputString);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your string: ");
+            string inputtedString = Console.ReadLine();
+            Console.WriteLine(onlyHasInt(inputtedString));
+        }
+    }
+}
